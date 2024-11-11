b6aa77adfa856239fee48e1e9fbf49c6d1051b76
DickarmienDeemie <ddeemie@scnlliberia.org>
2024-11-11 17:00:19 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const contactForm = document.getElementById('contact-form');
+    
+    contactForm.addEventListener('submit', function(event) {
+        event.preventDefault();
+        
+        // Basic form validation can be added here
+        
+        alert('Thank you for contacting us. We will get back to you
